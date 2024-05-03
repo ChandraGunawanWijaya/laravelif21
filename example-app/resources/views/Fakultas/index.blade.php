48a1d81fa167758325e40f7c7a30791cb5f65dde
Chandra <chandragunawanwijaya_2327250023@mhs.mdp.ac.id>
2024-05-03 20:31:44 +0700

    pushchandra

--- example-app/resources/views/Fakultas/index.blade.php
+++ example-app/resources/views/Fakultas/index.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     <h1>Universitas mdp</h1>
-      <a href="{{ url('fakultas') }}">List</a>  <a href="{{ url('fakultas/create') }}">Tambah</a>
-    <H2>Daftar fakultas</H2>
-    <p> ini halaman daftar fakultas </p>
-</body>
-</html>
+@extends('layout.main')
+
+@section('title', 'Daftar Fakultas')
+
+@section('content')
+<h2>Daftar Fakultas</h2>
+<p>Halaman Daftar Fakultas</p>
+@endsection
+
