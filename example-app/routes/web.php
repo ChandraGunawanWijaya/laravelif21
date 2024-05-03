c2c7fe6dfa8f8df9670eaec6590d98b798d63481
vantekzz <ivanarman311@gmail.com>
2024-05-03 20:13:17 +0700

    pp

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +15,5 @@ route::get('about', function(){
 route::get('profile',function(){
     return view('profile');
 });
+
+route::resource('fakultas', FakultasController::class);
