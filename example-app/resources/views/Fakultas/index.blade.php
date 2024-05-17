b289b664ce1ddf34d0e410414311740d4bcccba7
Chand <chandragunawanwijaya_2327250023@mhs.mdp.ac.id>
2024-05-17 20:20:57 +0700

    chan

--- example-app/resources/views/Fakultas/index.blade.php
+++ example-app/resources/views/Fakultas/index.blade.php
@@ -9,7 +9,37 @@
     {{$item['nama']}} {{$item['singkatan']}} <br>
     @endforeach
 
-    @endforeach
+    <div class="row">
+    <div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Daftar Fakultas</h4>
+                  <p class="card-description">
+                    List Data Fakultas
+                    Add class <code>.table-hover</code>
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th>Nama Fakultas</th>
+                          <th>Singkatan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                         @foreach ($fakultas as $item)
+                         <tr>
+                            <td>{{$item['nama']}}</td> <br>
+                            <td>{{$item['singkatan']}}</td> <br>
+                            </tr>
+                             @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
     @endsection
 
 
