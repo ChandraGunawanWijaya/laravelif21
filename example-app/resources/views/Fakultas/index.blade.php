79856610b3a0b626a65d28fa1558a6197ada6704
vantekzz <ivanarman311@gmail.com>
2024-05-17 20:31:33 +0700

    puahpyuah

--- example-app/resources/views/Fakultas/index.blade.php
+++ example-app/resources/views/Fakultas/index.blade.php
@@ -10,6 +10,39 @@
     @endforeach
 
     @endforeach
+
+    <div class="row">
+    <div class="col-lg-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Daftar Fakultas</h4>
+                  <p class="card-description">
+                    List Data Fakultas
+                    Add class <code>.table-hover</code>
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th>Nama Fakultas</th>
+                          <th>Singkatan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                         @foreach ($fakultas as $item)
+                         <tr>
+                           <td>{{$item['nama']}} </td><br>
+                           <td>{{$item['singkatan']}}</td><br>
+                         </tr>
+
+    @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
     @endsection
 
 
