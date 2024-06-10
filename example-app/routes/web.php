21660da3281f37bf7aa6ee9f3f54066b2b7f415c
jojoooook <ivanarman311@gmail.com>
2024-06-10 20:30:28 +0700

    JH

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProdiController;
 use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +19,7 @@ route::get('about', function(){
 route::get('profile',function(){
     return view('profile');
 });
+route::get('dashboard', [dashboard::class,'index']{
 
 route::resource('fakultas', FakultasController::class);
 route::resource('prodi', ProdiController::class);
